776d335c666284eac27d18bf092829d013b0f094
Adam Steinberg <adamsteinberg@Adams-Laptop.local>
2024-04-16 16:52:14 -0500

    finished the assignment

--- api-routes.js
+++ api-routes.js
@@ -12,8 +12,14 @@ const getCollection = async (dbName, collectionName) => {
 
 router.get("/", async (_, response) => {
   const collection = await getCollection("ToDoApi", "ToDo");
-  const todos = await collection.find({}).toArray(); // Find all items and convert to array
-  response.json(todos);
+  const todos = await collection.find({}).toArray() // Find all items and convert to array
+
+  const modifiedTodos = todos.map(td => {
+    const { _id, ...rest } = td; 
+    return { id: _id, ...rest };
+  });
+
+  response.json(modifiedTodos);
 });
 
 router.post("/", async (request, response) => {
@@ -25,23 +31,24 @@ router.post("/", async (request, response) => {
 
 // im getting a input must be a 24 char hex string??
 router.put("/:id", async (request, response) => {
-  const id = request.params.id.trim();
+  const id = request.params;
+  console.log(id, typeof id);
   const collection = await getCollection("ToDoApi", "ToDo");
 
-  const todo = await collection.findOne({ _id: new ObjectId(id) }); // Error the id isent hex string???
+  const todo = await collection.findOne({ "_id": new ObjectId(id) }); // Error the id isent hex string???
 
   if (!todo) {
     return response.status(404).json({ error: "Todo not found" });
   }
 
-  const updatedComplete = !todo.complete; // Toggle the 'complete' field
+  const updatedComplete = !todo.complete;
 
   const result = await collection.updateOne(
-    { _id: new ObjectId(id) },
-    { $set: { complete: updatedComplete } }, // Use updatedComplete here
+    { "_id": new ObjectId(id) },
+    { $set: { complete: updatedComplete } },
   );
 
-  response.json({ id: _id, complete: updatedComplete }); // Use updatedComplete here
+  response.json({ id: "_id", complete: updatedComplete });
 });
 
 module.exports = router;
